fb2ac7871f3587a70ac8483c92fd010d4b66ce86
AuroraSloan <lanajadethompson@gmail.com>
2022-01-03 23:39:43 +0900

    fixed error message for ft_cat. project complete

--- gopherdojo01/ft_cat/main.go
+++ gopherdojo01/ft_cat/main.go
@@ -3,32 +3,48 @@ package main
 import (
 	"io"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
+var exit_status = 0
+
 func main() {
-	exit_status := 0
 	if len(os.Args) < 2 {
-		print_file(os.Stdin, os.Stdout)
+		ft_cat(os.Stdin, os.Stdout)
 	}
 	for _, filename := range os.Args[1:] {
 		if filename == "-" {
-			print_file(os.Stdin, os.Stdout)
+			ft_cat(os.Stdin, os.Stdout)
 		} else {
 			src, err := os.Open(filename)
 			if err != nil {
-				exit_status = 1
+				print_err(err.Error())
 				continue
 			}
 			defer src.Close()
-			print_file(src, os.Stdout)
+			ft_cat(src, os.Stdout)
 		}
 	}
 	os.Exit(exit_status)
 }
 
-func print_file(src io.Reader, dst io.Writer) {
+func ft_cat(src io.Reader, dst io.Writer) {
 	_, err := io.Copy(dst, src)
 	if err != nil {
-		os.Exit(1)
+		print_err(err.Error())
 	}
+}
+
+func print_err(err string) {
+	exit_status = 1
+	error_message := filepath.Base(os.Args[0]) + ":"
+	words := strings.Fields(err)
+	for i, word := range words[1:] {
+		if i == 1 {
+			word = strings.Title(word)
+		}
+		error_message += " " + word
+	}
+	io.WriteString(os.Stderr, error_message+"\n")
 }
